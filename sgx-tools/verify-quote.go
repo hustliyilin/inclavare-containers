2e200f0a1a2ded7cfb681f29dde898f4c9222403
YiLin.Li <YiLin.Li@linux.alibaba.com>
2021-02-18 19:55:48 +0800

    sgx-tools: both support to verify epid and ecdsa quote
    Signed-off-by: Yilin Li <YiLin.Li@linux.alibaba.com>

--- sgx-tools/verify-quote.go
+++ sgx-tools/verify-quote.go
@@ -6,16 +6,23 @@ import (
 	"github.com/go-restruct/restruct"
 	"github.com/inclavare-containers/rune/libenclave/attestation"
 	"github.com/inclavare-containers/rune/libenclave/intelsgx"
+	"github.com/inclavare-containers/sgx-tools/pccs"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"io"
 	"log"
 	"os"
+	"strings"
+)
+
+const (
+	SgxEcdsaMinQuoteLength = 1020
+	SgxEpidMaxQuoteLength  = 2048
 )
 
 var verifyQuoteCommand = cli.Command{
 	Name:  "verify-quote",
-	Usage: "verify quote with the help of IAS",
+	Usage: "verify quote with the help of IAS(epid) or PCS(ecdsa)",
 	ArgsUsage: `[command options]
 
 EXAMPLE:
@@ -23,6 +30,10 @@ For example, get remote attestation report from IAS according to quote file:
 
 	# sgx-tools verify-quote --quote foo.quote --spid ${SPID} --subscription-key ${SUBSCRIPTION_KEY}`,
 	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "quote-type",
+			Usage: "specify the SGX quote type such as epidUnlinkable, epidLinkable and ecdsa",
+		},
 		cli.StringFlag{
 			Name:  "quote",
 			Usage: "path to the input quote file containing QUOTE",
@@ -37,19 +48,14 @@ For example, get remote attestation report from IAS according to quote file:
 		},
 	},
 	Action: func(context *cli.Context) error {
+		quoteType := context.String("quote-type")
+		if !strings.EqualFold(quoteType, intelsgx.QuoteTypeEcdsa) && !strings.EqualFold(quoteType, intelsgx.QuoteTypeEpidUnlinkable) && !strings.EqualFold(quoteType, intelsgx.QuoteTypeEpidLinkable) {
+			return fmt.Errorf("Unsupport quote type: %v", quoteType)
+		}
+
 		quotePath := context.String("quote")
 		if quotePath == "" {
 			return fmt.Errorf("quote argument cannot be empty")
-		}
-
-		spid := context.String("spid")
-		if spid == "" {
-			return fmt.Errorf("spid argument cannot be empty")
-		}
-
-		subscriptionKey := context.String("subscription-key")
-		if subscriptionKey == "" {
-			return fmt.Errorf("subscription-key argument cannot be empty")
 		}
 
 		if context.GlobalBool("verbose") {
@@ -71,46 +77,67 @@ For example, get remote attestation report from IAS according to quote file:
 			return err
 		}
 
-		if rfi.Size() > intelsgx.SgxMaxQuoteLength {
-			return fmt.Errorf("quote file %s not match Quote", quotePath)
-		}
-
 		quote := make([]byte, rfi.Size())
 		if _, err = io.ReadFull(rf, quote); err != nil {
 			return fmt.Errorf("quote file %s read failed", quotePath)
 		}
 
-		q := &intelsgx.Quote{}
-		if err := restruct.Unpack(quote, binary.LittleEndian, &q); err != nil {
-			return err
+		if strings.EqualFold(quoteType, intelsgx.QuoteTypeEpidUnlinkable) || strings.EqualFold(quoteType, intelsgx.QuoteTypeEpidLinkable) {
+			if rfi.Size() > SgxEpidMaxQuoteLength {
+				return fmt.Errorf("quote file %s not match epid quote", quotePath)
+			}
+
+			spid := context.String("spid")
+			if spid == "" {
+				return fmt.Errorf("spid argument cannot be empty")
+			}
+
+			subscriptionKey := context.String("subscription-key")
+			if subscriptionKey == "" {
+				return fmt.Errorf("subscription-key argument cannot be empty")
+			}
+
+			q := &intelsgx.Quote{}
+			if err := restruct.Unpack(quote, binary.LittleEndian, &q); err != nil {
+				return err
+			}
+
+			product, err := IsProductEnclave(q.ReportBody)
+			if err != nil {
+				return err
+			}
+
+			// get IAS remote attestation report
+			p := parseAttestParameters(spid, subscriptionKey, product)
+			challenger, err := attestation.NewChallenger("sgx-epid", p)
+			if err != nil {
+				log.Fatal(err)
+				return err
+			}
+
+			if err = challenger.Check(quote); err != nil {
+				log.Fatal(err)
+				return err
+			}
+
+			status, specificStatus, iasReport, err := challenger.GetReport(quote, 0)
+			if err != nil {
+				return fmt.Errorf("%s", err)
+			}
+
+			challenger.ShowReportStatus(status, specificStatus)
+
+			logrus.Infof("iasReport = %v", iasReport)
+		} else {
+			if rfi.Size() < SgxEcdsaMinQuoteLength {
+				return fmt.Errorf("quote file %s not match ecdsa quote", quotePath)
+			}
+
+			err = pccs.VerifyQuote(quote)
+			if err != nil {
+				return err
+			}
 		}
-
-		product, err := IsProductEnclave(q.ReportBody)
-		if err != nil {
-			return err
-		}
-
-		// get IAS remote attestation report
-		p := parseAttestParameters(spid, subscriptionKey, product)
-		challenger, err := attestation.NewChallenger("sgx-epid", p)
-		if err != nil {
-			log.Fatal(err)
-			return err
-		}
-
-		if err = challenger.Check(quote); err != nil {
-			log.Fatal(err)
-			return err
-		}
-
-		status, specificStatus, iasReport, err := challenger.GetReport(quote, 0)
-		if err != nil {
-			return fmt.Errorf("%s", err)
-		}
-
-		challenger.ShowReportStatus(status, specificStatus)
-
-		logrus.Infof("iasReport = %v", iasReport)
 
 		return nil
 	},
